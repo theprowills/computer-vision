acf11acec677a9d30c6eaa6fd85ce93563f7b0b5
Tsui Tin Lun <theprowills@gmail.com>
2023-11-28 14:58:30 +0700

    Add files via upload
    make a extraction image feature to a function format

--- Session 9/main.py
+++ Session 9/main.py
@@ -15,16 +15,34 @@ DATA_PATH = "data"
 
 features = []
 
-for file in os.listdir(DATA_PATH):
-    img_path = DATA_PATH + "/" + file
-    img_name = file.split(".")[0]
+# for file in os.listdir(DATA_PATH):
+#     img_path = DATA_PATH + "/" + file
+#     img_name = file.split(".")[0]
 
-    img_bgr = cv2.imread(img_path)
+#     img_bgr = cv2.imread(img_path)
+#     gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
+#     hist = cv2.calcHist([gray], [0], None, [8], [0, 256])
+#     normalized_hist = cv2.normalize(hist, None)
+
+#     flat_hist = normalized_hist.flatten()
+#     print(flat_hist, flat_hist.shape)
+
+#     features.append((img_name, flat_hist))
+
+
+# create a function for extracting feature
+def get_image_feature(path):
+    img_bgr = cv2.imread(path)
     gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
+
+    # comparing image in gray format
     hist = cv2.calcHist([gray], [0], None, [8], [0, 256])
+
+    # comparing image in color format (it might be more accurate)
+    # hist = cv2.calcHist([img_bgr], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+
     normalized_hist = cv2.normalize(hist, None)
 
     flat_hist = normalized_hist.flatten()
-    print(flat_hist, flat_hist.shape)
 
-    features.append((img_name, flat_hist))
+    return flat_hist
